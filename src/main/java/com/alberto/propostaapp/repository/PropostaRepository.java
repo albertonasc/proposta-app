c6df66f739481abcf610f6a152d2f6ca95acf2fb
Alberto Nascimento <beto.bdn@gmail.com>
2024-04-11 22:11:05 -0300

    Adição de PropostaRepository

---
+++ src/main/java/com/alberto/propostaapp/repository/PropostaRepository.java
@@ -0,0 +1,7 @@
+package com.alberto.propostaapp.repository;
+
+import com.alberto.propostaapp.entity.Proposta;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PropostaRepository extends CrudRepository<Proposta, Long> {
+}
